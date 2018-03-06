ecf0e92d28e03b11b71c6d4abc26b764447057a1
Starkus <emilianoalejosenega@gmail.com>
2018-03-06 14:09:53 -0300

    Animated hex tiles and empty modal

--- main.js
+++ main.js
@@ -24,6 +24,56 @@ moveItItem.prototype.update = function(scrollTop) {
     this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
 };
 
+var setUpModal = function() {
+    /* Picture modal */
+    var modal = $('#pictureModal')[0];
+
+    var modalImg = $('#img01');
+    var captionText = $('.caption');
+    var closeBtn = $('#pictureModal > .close')[0];
+
+    closeBtn.onclick = () => {
+        modal.style.display = "none";
+    }
+
+
+    /* Hex tiles animations and events */
+    $(window).scroll(function() {
+        const size = 150.0;
+        const shift = 100.0;
+
+        for (var hex of $('.hex')) {
+            const winHeight = $(window).height();
+            var top = hex.getBoundingClientRect().top;
+            var bot = -hex.getBoundingClientRect().bottom + winHeight;
+
+            top += shift;
+            bot += shift;
+
+            var opacity = Math.min(top, bot) / size;
+            hex.style.opacity = opacity;
+
+            /* A sort of "bubble" function for the scale */
+            var scale = Math.min(top, bot) / (winHeight / 2);
+            scale = Math.sin(scale * 1.57);
+            scale = Math.min(1, scale + 0.6);
+
+            hex.style.transform = 'rotate(120deg) scale(' + scale + ')';
+            console.log(hex.style.transform.scale);
+
+            /* On click, open modal */
+            hex.onclick = () => {
+                console.log(modal);
+                modal.style.display = "block";
+                modalImg.src = "/images/thumbnail-clio.jpg";
+            }
+        }
+    })
+}
+
+
 $(document).ready(function() {
     $('[data-scroll-speed]').moveIt();
+
+    setUpModal();
 });
